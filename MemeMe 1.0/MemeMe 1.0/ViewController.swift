13cc5c6cdfbccbe40112c200e2d4b6f33ca1ee38
mes32 <mes32@noreply.github.com>
2016-09-04 13:04:49 -0400

    Initialized xcode project for this application

---
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Michael Stockman on 9/4/16.
+//  Copyright © 2016 Michael Stockman.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
