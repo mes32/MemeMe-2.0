103d63325fb4486fdd66a23023f6d58924612c39
mes32 <stockman.mike@gmail.com>
2016-09-09 09:22:42 -0400

    feature: Implement image picking from photo album and camera (integrate code from pick-image branch)

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -10,23 +10,21 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //saveButton.enabled = false
-        //cameraButton.enabled = false
+        imagePicker.delegate = self
+        saveButton.enabled = false
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
-
-    /*override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }*/
 
     @IBAction func pressedSaveButton(sender: AnyObject) {
         print("Pressed save button")
@@ -34,77 +32,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func pressedCameraButton(sender: AnyObject) {
         print("Pressed camera button")
-    }
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)    }
     
     @IBAction func pressedPhotoAlbumButton(sender: AnyObject) {
-        print("Pressed photo album button")
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            backgroundView.backgroundColor = UIColor.blackColor()
+            imageView.image = pickedImage
+            imageView.sizeToFit()
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
+    func imagePickerControllerDidCancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
-    /*
- 
-     //
-     //  ViewController.swift
-     //  MemeMe 1.0
-     //
-     //  Created by Michael Stockman on 9/4/16.
-     //  Copyright © 2016 Michael Stockman.
-     //
-     
-     import UIKit
-     
-     class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-     
-     @IBOutlet weak var imageView: UIImageView!
-     @IBOutlet weak var cameraButton: UIButton!
-     let imagePicker = UIImagePickerController()
-     
-     override func viewDidLoad() {
-     super.viewDidLoad()
-     imagePicker.delegate = self
-     cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
-     }
-     
-     override func didReceiveMemoryWarning() {
-     super.didReceiveMemoryWarning()
-     // Dispose of any resources that can be recreated.
-     }
-     
-     @IBAction func PhotoAlbumButton(sender: AnyObject) {
-     print("Pressed Photo Album Button")
-     imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-     presentViewController(imagePicker, animated: true, completion: nil)
-     }
-     
-     @IBAction func CameraButton(sender: AnyObject) {
-     print("Pressed Camera Button")
-     imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-     presentViewController(imagePicker, animated: true, completion: nil)
-     }
-     
-     /*@IBAction func pickAnImage(sender: AnyObject) {
-     
-     }*/
-     
-     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-     if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-     imageView.image = pickedImage
-     //imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-     imageView.sizeToFit()
-     }
-     
-     dismissViewControllerAnimated(true, completion: nil)
-     }
-     
-     func imagePickerControllerDidCancel() {
-     dismissViewControllerAnimated(true, completion: nil)
-     }
-     }
-     
-
- 
-     */
-
 }
 
