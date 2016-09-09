e1ddf86a985575bea576d7d9f2a687ab0b94241a
mes32 <stockman.mike@gmail.com>
2016-09-09 08:44:55 -0400

    feature: Add outlets and actions for the save button, the camera button, and the photo album button

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //saveButton.enabled = false
+        //cameraButton.enabled = false
     }
 
-    override func didReceiveMemoryWarning() {
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }*/
 
+    @IBAction func pressedSaveButton(sender: AnyObject) {
+        print("Pressed save button")
+    }
+    
+    @IBAction func pressedCameraButton(sender: AnyObject) {
+        print("Pressed camera button")
+    }
+    
+    @IBAction func pressedPhotoAlbumButton(sender: AnyObject) {
+        print("Pressed photo album button")
+    }
 
 }
 
