bace3c27ef74d98c2a42c7a83c813a931d057c91
mes32 <stockman.mike@gmail.com>
2016-09-25 11:03:33 -0400

    feature: Add rudimentary sent memes view

---
+++ MemeMe 2.0/MemeMe 2.0/SentMemesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SentMemesViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Onyinyechukwu Uchime on 9/25/16.
+//  Copyright © 2016 Michael Stockman. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("SentMemesViewController viewDidLoad()")
+    }
+    
+}
